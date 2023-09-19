4fbaf70b72acc9863bd8761d39736990971500f7
RodSoriano <rsoriano@ioogo.com>
2023-09-19 15:41:45 -0600

    Update tweet model with a better structure of code

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,10 +6,9 @@ class Tweet < ApplicationRecord
     'Thread'
   }
 
-  belongs_to :user, class_name: User, foreign_key: :user_id
-
-  belongs_to :tweet, class_name: Tweet
   has_many :retweets, class_name: Tweet
-
+  belongs_to :tweet, class_name: Tweet
   has_many :likes, class_name: Like
+
+  belongs_to :user, class_name: User, foreign_key: :user_id
 end

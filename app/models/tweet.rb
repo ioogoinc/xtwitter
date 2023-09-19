15a091453856c87cebd627840dcbd97e1fde3f0c
RodSoriano <rsoriano@ioogo.com>
2023-09-19 15:25:06 -0600

    Update tweet model with its relationships.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,13 @@
 class Tweet < ApplicationRecord
+  enum type: {
+    'Quote',
+    'Reply',
+    'Retweet',
+    'Thread'
+  }
+
+  belongs_to :user, class_name: User, foreign_key: :user_id
+
+  belongs_to :tweet, class_name: Tweet
+  has_many :retweet, class_name: Tweet
 end

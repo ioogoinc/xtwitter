b280e8f8f5c501b748688a3a74574b64d9ff0263
RodSoriano <rsoriano@ioogo.com>
2023-09-19 15:50:36 -0600

    Update tweet with bookmark relationship.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -9,6 +9,7 @@ class Tweet < ApplicationRecord
   has_many :retweets, class_name: Tweet
   belongs_to :tweet, class_name: Tweet
   has_many :likes, class_name: Like
+  belongs_to :bookmark, class_name: Bookmark
 
   belongs_to :user, class_name: User, foreign_key: :user_id
 end

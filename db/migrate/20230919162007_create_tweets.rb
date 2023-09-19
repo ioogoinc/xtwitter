e46d11e5a1316aef61831dd8958197b04b6842b9
RodSoriano <rsoriano@ioogo.com>
2023-09-19 11:24:49 -0600

    Update migration of tweets table.

--- db/migrate/20230919162007_create_tweets.rb
+++ db/migrate/20230919162007_create_tweets.rb
@@ -2,11 +2,13 @@ class CreateTweets < ActiveRecord::Migration[7.0]
   def change
     create_table :tweets do |t|
       add_reference :users, :user, foreign_key: true 
-      add_reference :tweets, :tweet, foreign_key: true
+      t.integer :tweet_id # Needs more research on applying foreign key after creation of table
       t.string :type # enum: [retweet, reply, quote, thread]
       t.text :body, limit: 480
 
       t.timestamps
     end
+
+    change_column :tweets, :tweet_id, :integer, null: true
   end
 end

df414e41b895e73889639e72c42b587cb7e9237c
RodSoriano <rsoriano@ioogo.com>
2023-09-19 10:57:51 -0600

    Update user migrations email and comment for password.

--- db/migrate/20230919150945_create_users.rb
+++ db/migrate/20230919150945_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :email
-      t.string :password, comment: 'This data type will be changed on validations.'
+      t.string :email, unique: true
+      t.string :password, comment: 'The data type of this column will be changed on validations.'
       t.boolean :private
 
       t.timestamps

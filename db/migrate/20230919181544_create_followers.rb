dad0686b38da1155968bcb3fee5edfdace4a8867
RodSoriano <rsoriano@ioogo.com>
2023-09-19 14:28:08 -0600

    Update followers migration file.

--- db/migrate/20230919181544_create_followers.rb
+++ db/migrate/20230919181544_create_followers.rb
@@ -3,11 +3,10 @@ class CreateFollowers < ActiveRecord::Migration[7.0]
     create_table :followers do |t|
       t.integer :follow_id
       t.integer :follower_id
-
-      add_reference :users, :follow_id, foreign_key: { to_table: :users }
-      add_reference :users, :follower_id, foreign_key: { to_table: :users }
-
       t.timestamps
     end
+
+    add_foreign_key :followers, :users, column: :follow_id
+    add_foreign_key :followers, :users, column: :follower_id
   end
 end

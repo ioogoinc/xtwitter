98efa97e9c45b8a28db8c1c899eb10addecba029
RodSoriano <rsoriano@ioogo.com>
2023-09-19 14:10:06 -0600

    File with previous methods was not defining the fields into the shcema rails file so I had to declare the fields before adding the reference.

--- db/migrate/20230919181544_create_followers.rb
+++ db/migrate/20230919181544_create_followers.rb
@@ -1,8 +1,11 @@
 class CreateFollowers < ActiveRecord::Migration[7.0]
   def change
     create_table :followers do |t|
-      add_reference :users, :follow, foreign_key: { to_table: :users }
-      add_reference :users, :follower, foreign_key: { to_table: :users }
+      t.integer :follow_id
+      t.integer :follower_id
+
+      add_reference :users, :follow_id, foreign_key: { to_table: :users }
+      add_reference :users, :follower_id, foreign_key: { to_table: :users }
 
       t.timestamps
     end

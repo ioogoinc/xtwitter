0b3637e97f275721ed2f7560ceffed9a373fd7cd
RodSoriano <rsoriano@ioogo.com>
2023-09-19 13:48:17 -0600

    Update order of table twitters to follow db conventions.

--- db/migrate/20230919162007_create_tweets.rb
+++ db/migrate/20230919162007_create_tweets.rb
@@ -1,10 +1,10 @@
 class CreateTweets < ActiveRecord::Migration[7.0]
   def change
     create_table :tweets do |t|
+      t.references :users, foreign_key: true, index: false
       t.integer :other_tweet_id, null: true
       t.string :type # enum: [retweet, reply, quote, thread]
       t.text :body, limit: 480
-      t.references :users, foreign_key: true, index: false
 
       t.timestamps
     end

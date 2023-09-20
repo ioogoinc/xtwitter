9058a775ecf440d31a19e0bc507fc39e7e7070eb
RodSoriano <rsoriano@ioogo.com>
2023-09-20 09:14:02 -0600

    Update likes relationship no neccesity to explicity call class_name since the class follows naming conventions.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-    belongs_to :tweet, class_name: Tweet, foreign_key: :tweet_id
-    belongs_to :user, class_name: User, foreign_key: :user_id
+    belongs_to :tweet
+    belongs_to :user
 end

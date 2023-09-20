52ae400e55c67a5d252eeebb811c7150f49d32cb
RodSoriano <rsoriano@ioogo.com>
2023-09-20 09:26:27 -0600

    Update bookmark model code due to naming convention usage.

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,4 @@
 class Bookmark < ApplicationRecord
-    bolongs_to :user, class_name: User, foreign_key: :user_id
-    has_one :tweet, class_name: Tweet, foreign_key: :tweet_id
+    bolongs_to :user
+    has_one :tweet
 end

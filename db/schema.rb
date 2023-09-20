ba17ae6d22b571c56519bcf7dbb64a2a8abf46e6
MelissaPLeitez <mramirez@ioogo.com>
2023-09-20 14:36:48 -0600

    Adding Bookmark model with its User and Tweet references, also adding Hashtag model with its pivot association with model Tweet

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_191928) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_202546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "tweet_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tweet_id"], name: "index_bookmarks_on_tweet_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
 
   create_table "follows", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "follower_id"
     t.integer "followee_id"
+    t.index ["follower_id", "followee_id"], name: "index_follows_on_follower_id_and_followee_id", unique: true, comment: "Unique index to make sure that the user can not\n    follow the same user more than one time"
+  end
+
+  create_table "hashtags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hashtags_tweets", id: false, force: :cascade do |t|
+    t.bigint "hashtag_id", null: false
+    t.bigint "tweet_id", null: false
   end
 
   create_table "likes", force: :cascade do |t|
@@ -50,6 +71,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_191928) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookmarks", "tweets"
+  add_foreign_key "bookmarks", "users"
   add_foreign_key "likes", "tweets"
   add_foreign_key "likes", "users"
   add_foreign_key "tweets", "users"

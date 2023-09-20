25186befb717a3356df45a926ad3a3c2e7f2abc9
danlopz7 <dlopez@ioogo.com>
2023-09-20 16:00:34 -0600

    migration file changed

--- db/migrate/20230920215042_rename_columns_in_follow.rb
+++ db/migrate/20230920215042_rename_columns_in_follow.rb
@@ -1,4 +1,6 @@
 class RenameColumnsInFollow < ActiveRecord::Migration[7.0]
   def change
+    rename_column :follows, :follower, :follower_id
+    rename_column :follows, :followee, :followee_id
   end
 end

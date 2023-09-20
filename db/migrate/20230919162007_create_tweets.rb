1bcfddb52b33c6f9ce11439fbe5fd3a70b651625
RodSoriano <rsoriano@ioogo.com>
2023-09-20 09:52:55 -0600

    Update tweets table migration with proper self reference.

--- db/migrate/20230919162007_create_tweets.rb
+++ db/migrate/20230919162007_create_tweets.rb
@@ -2,7 +2,7 @@ class CreateTweets < ActiveRecord::Migration[7.0]
   def change
     create_table :tweets do |t|
       t.references :users, foreign_key: true
-      t.integer :other_tweet_id, null: true
+      t.references :other_tweet_id, { to_table: :tweets }, null: true
       t.string :type
       t.text :body, limit: 480
 

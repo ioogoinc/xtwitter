7fbfedbd607a369779800bfeedf313f5ba6ad4b2
RodSoriano <rsoriano@ioogo.com>
2023-09-20 09:24:48 -0600

    Update pivot table removing amount of unneccesary code due to the usage of naming conventions for the models.

--- app/models/hashtag_tweet.rb
+++ app/models/hashtag_tweet.rb
@@ -1,4 +1,4 @@
 class HashtagTweet < ApplicationRecord
-    belongs_to :hashtag, class_name: Hashtag, foreign_key: :hashtag_id
-    belongs_to :tweet, class_name: Tweet, foreign_key: :tweet_id
+    belongs_to :hashtag
+    belongs_to :tweet
 end

2c2068d1c903102699f31cabdfcbab451460784b
Phairful <fpaolacsolis@gmail.com>
2023-09-21 10:34:23 -0600

    quote validation

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,6 @@
 class Quote < ApplicationRecord
   belongs_to :bookmarked_tweet, class_name: "Tweet"
   belongs_to :bookmarking_user, class_name: "User"
+  validates :quote_body, length: {within: (1...255)}, presence: { message: "must be given please" }
+  
 end

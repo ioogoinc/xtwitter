785008b861a1af7989475c776268ee3e35aa295a
MelissaPLeitez <mramirez@ioogo.com>
2023-09-21 09:43:04 -0600

    Adding User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
-class User < ApplicationRecor 
-    has_many :followers, class_name: follow, foreign_key: :follower_id
-    has_many :followees, class_name: follow, foreign_key: :followee_id
+class User < ApplicationRecord
+    has_many :followers, class_name: 'follow', foreign_key: :follower_id
+    has_many :followees, class_name: 'follow', foreign_key: :followee_id
+
+    validates :email, :username, presence: true, 
+        uniqueness: true
+    validates :password,
+        presence: true, 
+        length: {minimum: 12}, 
+        format: { with: /(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+])/ }
 end
+                                                             

73126d82e6cee0bf28cd70dd79cd8e4c75c24be2
MelissaPLeitez <mramirez@ioogo.com>
2023-09-21 10:23:10 -0600

    Adding Tweet validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,12 @@ class Tweet < ApplicationRecord
     belongs_to :user
     has_many :retweets, class_name: 'Tweet', foreign_key: :retweet_id
     has_many :quotes, class_name: 'Tweet', foreign_key: :quote_id
+
+    validates :body, length: {maximum: 255}
+    validates :body, presence: true, if: :tweet_or_quote?
+
+    def tweet_or_quote?
+        retweet_id_id.nil?
+    end
+
 end

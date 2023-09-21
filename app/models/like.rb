c90947229d9795e5282a26ebaecbce29b2daf5fb
Phairful <fpaolacsolis@gmail.com>
2023-09-21 10:19:38 -0600

    likes validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
     belongs_to :liking_user, class_name: "User"
     belongs_to :liked_tweet, class_name: "Tweet"
+    validates :liked_tweet_id, uniqueness: true
+    validates :liking_user_id, uniqueness: true
 end

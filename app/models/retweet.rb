c15d2c8f0a3e1a41d3cc992e820f4d33ce826b4a
Phairful <fpaolacsolis@gmail.com>
2023-09-21 10:23:47 -0600

    ReTweet Validation

--- app/models/retweet.rb
+++ app/models/retweet.rb
@@ -1,4 +1,6 @@
 class Retweet < ApplicationRecord
   belongs_to :retweeting_user, class_name: "User"
   belongs_to :retweeted_tweet, class_name: "Tweet"
+  validates :retweed_tweet_id, uniqueness: true
+  validates :retweeting_user_id, uniqueness: true
 end

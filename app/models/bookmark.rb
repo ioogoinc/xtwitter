fec4ffd9804b0a5af5cdb33222809df859135f92
Phairful <fpaolacsolis@gmail.com>
2023-09-21 10:25:25 -0600

    bookmark validation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :bookmarked_tweet, class_name: "Tweet"
   belongs_to :bookmarking_user, class_name: "User"
+  validates :bookmarked_tweet_id, uniqueness: true
+  validates :bookmarking_user_id, uniqueness: true
 end

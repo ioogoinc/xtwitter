55e54546e42ff934a507096e0778f9fd09d48d15
MelissaPleitez <melissa.ramirez0pleitez@gmail.com>
2023-09-25 16:04:43 -0600

    Updating follow by adding some Validations

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Follow, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "association" do
+    it { should belong_to(:follower).class_name("User").with_foreign_key(:follower_id) } 
+    it { should belong_to(:followee).class_name("User").with_foreign_key(:followee_id) } 
+  end
+
+  context "validations" do
+    it { should validate_presence_of(:follower_id) }
+    it { should validate_presence_of(:followee_id) }
+    it { should validate_uniqueness_of(:follower_id).scoped_to(:followee_id) }
+  end
 end
